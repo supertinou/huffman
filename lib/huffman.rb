5415a5bcb49ce693364d4b11f6cacc8f9ff7a9fc
Martin Lagrange <lagrangemartin@gmail.com>
2013-12-17 00:14:22 +0100

    We can now decode and encode a text  ! Yeah

--- lib/huffman.rb
+++ lib/huffman.rb
@@ -10,11 +10,24 @@ module Huffman
 	def encode_text(txt)
 		frequencies = LetterFrequency.get_frequencies(txt)
 		tree = Tree.new(frequencies)
-		tree.display_as_png()		 
+		tree.display_as_png()
+		dictionnary = tree.dictionnary
+		encoded_text = txt.each_char.map{|char| dictionnary.invert[char]}.join		 
+		return encoded_text, dictionnary
 	end
 
-	def decode_text(txt)
-		
+	def decode_text(encoded_text,dictionnary)
+		original_text = ''
+		buffer = ''
+		encoded_text.each_char do |byte| 
+			buffer += byte
+			# Si il y'a une correspondance
+			if dictionnary[buffer] 
+				 original_text += dictionnary[buffer] 
+				 buffer.clear
+			end
+		end
+		original_text
 	end
 
 
--- spec/lib/huffman_spec.rb
+++ spec/lib/huffman_spec.rb
@@ -2,10 +2,10 @@ require "spec_helper.rb"
 
 module Huffman
 	describe "HuffmanCoder" do 
-		xit "decode an encoded string and get back the original string" do
+		it "decode an encoded string and get back the original string" do
 			 txt = "La vie est un long fleuve tranquille avec des impasses, des courants de mer"	
 	  		 encoded_text, dictionnary = Huffman.encode_text(txt)
-	  		 new_text = Huffman.decode_text(encoded_text,dictionnary)
+	  		 new_txt = Huffman.decode_text(encoded_text,dictionnary)
 	  		 new_txt.should == txt
 		end
 	end
